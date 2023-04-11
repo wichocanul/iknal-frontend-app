61a81d187d8263a2624bc70edda13e8bd7c1f3a0
wicho <wichocanul07@gmail.com>
2023-04-11 16:00:04 -0600

    Iknal: create header component to home section

--- src/app/clientApp/pages/home/home.module.ts
+++ src/app/clientApp/pages/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeMainComponent } from './home-main/home-main.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeMainComponent
+    HomeMainComponent,
+    HomeHeaderComponent
   ],
   imports: [
     CommonModule
